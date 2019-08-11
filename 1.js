7319c97adbd3aee84a385afb0fe6893bcc4817ad
hungpitt <tranduyhung0210@gmail.com>
2019-08-11 14:16:30 +0700

    len nao

--- 1.js
+++ 1.js
@@ -1,4 +1,10 @@
 ﻿ $(function(){
- 
+ 	vitricuaedu = $('#khoiedu').offset().top ;
+ 	
+
+
+ 	$('.xuong,.kn').click(function(event) {
+ 		$('body,html').animate({scrollTop:vitricuaedu},600)
+ 	});
 })  
  
